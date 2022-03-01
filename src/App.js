3e2615e3a9f678f58e8b081d205d39471500f4b1
trinhquangha <trinhha732000@gmail.com>
2022-03-01 23:07:54 +0700

    Design Trello Frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,116 @@
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="trello-app">Trello</h1>
-    </div>
-  );
+	return (
+		<div className="trello-app">
+			<nav className="navbar app">App navbar</nav>
+			<nav className="navbar board">Board navbar</nav>
+			<div className="board-columns">
+				<div className="column">
+					<header>Title</header>
+					<ul>
+						<li>
+							<img
+								src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/272900017_1527660794283846_2968176870957752193_n.jpg?stp=dst-jpg_s600x600&_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yhtlaH51qTkAX_dB7mM&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-wqipor5-JSxRy726c6wtCUmetm3VXCTGHZsZO9HBYMw&oe=62235185"
+								alt="image"
+							/>
+						</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>Title</header>
+					<ul>
+						<li>
+							<img
+								src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/272900017_1527660794283846_2968176870957752193_n.jpg?stp=dst-jpg_s600x600&_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yhtlaH51qTkAX_dB7mM&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-wqipor5-JSxRy726c6wtCUmetm3VXCTGHZsZO9HBYMw&oe=62235185"
+								alt="image"
+							/>
+						</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>Title</header>
+					<ul>
+						<li>
+							<img
+								src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/272900017_1527660794283846_2968176870957752193_n.jpg?stp=dst-jpg_s600x600&_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yhtlaH51qTkAX_dB7mM&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-wqipor5-JSxRy726c6wtCUmetm3VXCTGHZsZO9HBYMw&oe=62235185"
+								alt="image"
+							/>
+						</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>Title</header>
+					<ul>
+						<li>
+							<img
+								src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/272900017_1527660794283846_2968176870957752193_n.jpg?stp=dst-jpg_s600x600&_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yhtlaH51qTkAX_dB7mM&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-wqipor5-JSxRy726c6wtCUmetm3VXCTGHZsZO9HBYMw&oe=62235185"
+								alt="image"
+							/>
+						</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>Title</header>
+					<ul>
+						<li>
+							<img
+								src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/272900017_1527660794283846_2968176870957752193_n.jpg?stp=dst-jpg_s600x600&_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yhtlaH51qTkAX_dB7mM&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-wqipor5-JSxRy726c6wtCUmetm3VXCTGHZsZO9HBYMw&oe=62235185"
+								alt="image"
+							/>
+						</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>Title</header>
+					<ul>
+						<li>
+							<img
+								src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/272900017_1527660794283846_2968176870957752193_n.jpg?stp=dst-jpg_s600x600&_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yhtlaH51qTkAX_dB7mM&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-wqipor5-JSxRy726c6wtCUmetm3VXCTGHZsZO9HBYMw&oe=62235185"
+								alt="image"
+							/>
+						</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+						<li>Add something</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
