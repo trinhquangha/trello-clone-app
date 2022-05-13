b8f7d4d7dbc93187545902d411bba1d4e979c202
trinhquangha <trinhha732000@gmail.com>
2022-05-14 00:37:28 +0700

    Combine APIs drag & drop columns, cards

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -16,9 +16,15 @@ import Column from 'components/Column/Column';
 import { mapOrder } from 'utilities/sorts';
 import { applyDrag } from 'utilities/dragDrop';
 
-import { isEmpty } from 'lodash';
+import { isEmpty, cloneDeep, isEqual } from 'lodash';
 
-import { fetchBoardDetail, createNewColumn } from 'actions/apiCall';
+import {
+	fetchBoardDetail,
+	createNewColumn,
+	updateBoard,
+	updateColumn,
+	updateCard,
+} from 'actions/apiCall';
 
 const BoardContent = () => {
 	const [board, setBoard] = useState({});
@@ -48,26 +54,65 @@ const BoardContent = () => {
 	}
 
 	const onColumnDrop = (dropResult) => {
-		let newColumns = [...columns];
+		let newColumns = cloneDeep(columns);
 		newColumns = applyDrag(newColumns, dropResult);
 
-		const newBoard = { ...board };
+		const newBoard = cloneDeep(board);
 		newBoard.columnOrder = newColumns.map((column) => column._id);
 		newBoard.columns = newColumns;
 
 		setColumns(newColumns);
 		setBoard(newBoard);
+
+		// Call API only if the columnOrder is updated
+		if (!isEqual(board.columnOrder, newBoard.columnOrder)) {
+			// Call API update columnOrder in board detail
+			updateBoard(newBoard._id, newBoard).catch(() => {
+				setColumns(columns);
+				setBoard(board);
+			});
+		}
 	};
 
 	const onCardDrop = (columnId, dropResult) => {
 		if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
-			let newColumns = [...columns];
+			let newColumns = cloneDeep(columns);
 
 			let currentColumn = newColumns.find((column) => column._id === columnId);
 			currentColumn.cards = applyDrag(currentColumn.cards, dropResult);
 			currentColumn.cardOrder = currentColumn.cards.map((card) => card._id);
 
 			setColumns(newColumns);
+
+			if (!isEqual(columns, newColumns)) {
+				if (
+					dropResult.removedIndex !== null &&
+					dropResult.addedIndex !== null
+				) {
+					/*
+					 * Action: move card inside its column
+					 * Call API update cardOrder in current column
+					 */
+					updateColumn(currentColumn._id, currentColumn).catch(() =>
+						setColumns(columns)
+					);
+				} else {
+					/*
+					 * Action: move card between 2 columns
+					 * 1 - Call API update cardOrder in current column
+					 */
+					updateColumn(currentColumn._id, currentColumn).catch(() =>
+						setColumns(columns)
+					);
+
+					if (dropResult.addedIndex !== null) {
+						let currentCard = cloneDeep(dropResult.payload);
+						currentCard.columnId = currentColumn._id;
+            // * 2 - Call API update columnId in current card
+						updateCard(currentCard._id, currentCard);
+					}
+				}
+			}
 		}
 	};
 
@@ -85,19 +130,19 @@ const BoardContent = () => {
 			title: newColumnTitle,
 		};
 
-    //Call API
-		createNewColumn(newColumnToAdd).then(column => {
-      const newColumns = [...columns];
-      newColumns.push(column);
+		//Call API
+		createNewColumn(newColumnToAdd).then((column) => {
+			const newColumns = [...columns];
+			newColumns.push(column);
 
-      const newBoard = { ...board };
-      newBoard.columnOrder = newColumns.map((col) => col._id);
-      newBoard.columns = newColumns;
+			const newBoard = { ...board };
+			newBoard.columnOrder = newColumns.map((col) => col._id);
+			newBoard.columns = newColumns;
 
-      setColumns(newColumns);
-      setBoard(newBoard);
-      setOpenNewColumnForm(!openNewColumnForm);
-    })
+			setColumns(newColumns);
+			setBoard(newBoard);
+			setOpenNewColumnForm(!openNewColumnForm);
+		});
 	};
 
 	const onUpdateColumn = (newColumnToUpdate) => {
