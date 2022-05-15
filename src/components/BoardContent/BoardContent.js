ebba23c3e23bf2adf8a6347e939907a973751867
trinhquangha <trinhha732000@gmail.com>
2022-05-15 23:56:23 +0700

    Add Loader

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import './BoardContent.scss';
-
+import ReactLoading from 'react-loading';
 import { Container, Draggable } from 'react-smooth-dnd';
-
 import {
 	Container as BootstrapContainer,
 	Row,
@@ -10,13 +8,14 @@ import {
 	Form,
 	Button,
 } from 'react-bootstrap';
+import { isEmpty, cloneDeep, isEqual } from 'lodash';
+
+import './BoardContent.scss';
 
 import Column from 'components/Column/Column';
 
 import { mapOrder } from 'utilities/sorts';
 import { applyDrag } from 'utilities/dragDrop';
-
-import { isEmpty, cloneDeep, isEqual } from 'lodash';
 
 import {
 	fetchBoardDetail,
@@ -31,12 +30,14 @@ const BoardContent = () => {
 	const [columns, setColumns] = useState([]);
 	const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
 	const [newColumnTitle, setNewColumnTitle] = useState('');
+	const [isLoaded, setIsLoaded] = useState(false);
 
 	const newColumnInputRef = useRef(null);
 
 	useEffect(() => {
 		const boardId = '62375035b1a588be64ed25e9';
 		fetchBoardDetail(boardId).then((board) => {
+			setIsLoaded(true);
 			setBoard(board);
 			setColumns(mapOrder(board.columns, board.columnOrder, '_id'));
 		});
@@ -48,10 +49,6 @@ const BoardContent = () => {
 			newColumnInputRef.current.select();
 		}
 	}, [openNewColumnForm]);
-
-	if (isEmpty(board)) {
-		return <div className="not-found">Board not found</div>;
-	}
 
 	const onColumnDrop = (dropResult) => {
 		let newColumns = cloneDeep(columns);
@@ -108,7 +105,7 @@ const BoardContent = () => {
 					if (dropResult.addedIndex !== null) {
 						let currentCard = cloneDeep(dropResult.payload);
 						currentCard.columnId = currentColumn._id;
-            // * 2 - Call API update columnId in current card
+						// * 2 - Call API update columnId in current card
 						updateCard(currentCard._id, currentCard);
 					}
 				}
@@ -168,66 +165,78 @@ const BoardContent = () => {
 		setBoard(newBoard);
 	};
 
-	return (
-		<div className="board-content">
-			<Container
-				orientation="horizontal"
-				onDrop={onColumnDrop}
-				getChildPayload={(index) => columns[index]}
-				dragHandleSelector=".column-drag-handle"
-				dropPlaceholder={{
-					animationDuration: 150,
-					showOnTop: true,
-					className: 'column-drop-preview',
-				}}>
-				{columns.map((column, index) => (
-					<Draggable key={index}>
-						<Column
-							column={column}
-							onCardDrop={onCardDrop}
-							onUpdateColumn={onUpdateColumn}
-						/>
-					</Draggable>
-				))}
-			</Container>
-			<BootstrapContainer className="container">
-				{!openNewColumnForm && (
-					<Row>
-						<Col
-							className="add-another-column"
-							onClick={toggleOpenNewColumnForm}>
-							<i className="fa fa-plus icon"></i>Add another column
-						</Col>
-					</Row>
-				)}
-
-				{openNewColumnForm && (
-					<Row>
-						<Col className="enter-new-column">
-							<Form.Control
-								size="sm"
-								type="text"
-								placeholder="Enter column title..."
-								className="input-new-column"
-								ref={newColumnInputRef}
-								value={newColumnTitle}
-								onChange={(e) => setNewColumnTitle(e.target.value)}
-								onKeyDown={(e) => e.key === 'Enter' && addNewColumn()}
+	if (!isLoaded) {
+		return (
+			<div className="loader">
+				<ReactLoading type={'bars'} height={80} width={80} />
+			</div>
+		);
+	} else if (isEmpty(board)) {
+		return <div>Board not found</div>;
+	} else {
+		return (
+			<div className="board-content">
+				<Container
+					orientation="horizontal"
+					onDrop={onColumnDrop}
+					getChildPayload={(index) => columns[index]}
+					dragHandleSelector=".column-drag-handle"
+					dropPlaceholder={{
+						animationDuration: 150,
+						showOnTop: true,
+						className: 'column-drop-preview',
+					}}>
+					{columns.map((column, index) => (
+						<Draggable key={index}>
+							<Column
+								column={column}
+								onCardDrop={onCardDrop}
+								onUpdateColumn={onUpdateColumn}
 							/>
-							<div className="add-column-control">
-								<Button size="sm" variant="success" onClick={addNewColumn}>
-									Add column
-								</Button>
-								<span className="cancel-icon" onClick={toggleOpenNewColumnForm}>
-									<i className="fa fa-trash icon"></i>
-								</span>
-							</div>
-						</Col>
-					</Row>
-				)}
-			</BootstrapContainer>
-		</div>
-	);
+						</Draggable>
+					))}
+				</Container>
+				<BootstrapContainer className="container">
+					{!openNewColumnForm && (
+						<Row>
+							<Col
+								className="add-another-column"
+								onClick={toggleOpenNewColumnForm}>
+								<i className="fa fa-plus icon"></i>Add another column
+							</Col>
+						</Row>
+					)}
+
+					{openNewColumnForm && (
+						<Row>
+							<Col className="enter-new-column">
+								<Form.Control
+									size="sm"
+									type="text"
+									placeholder="Enter column title..."
+									className="input-new-column"
+									ref={newColumnInputRef}
+									value={newColumnTitle}
+									onChange={(e) => setNewColumnTitle(e.target.value)}
+									onKeyDown={(e) => e.key === 'Enter' && addNewColumn()}
+								/>
+								<div className="add-column-control">
+									<Button size="sm" variant="success" onClick={addNewColumn}>
+										Add column
+									</Button>
+									<span
+										className="cancel-icon"
+										onClick={toggleOpenNewColumnForm}>
+										<i className="fa fa-trash icon"></i>
+									</span>
+								</div>
+							</Col>
+						</Row>
+					)}
+				</BootstrapContainer>
+			</div>
+		);
+	}
 };
 
 export default BoardContent;
